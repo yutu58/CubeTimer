3e3bcf8b0c06762453044ce419e5ece41cc1f060
yutu58 <165717@driestarleerling.nl>
2019-09-24 19:10:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -98,7 +98,7 @@ var status = 0;
             if (event.keyCode == 32) {
                 if(status == 1) {
                     stop();
-		generateScramble();
+		generateScramble;
 		}
  				if (timertrue == 0) {
                 timertrue = 1;
