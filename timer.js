13a5d5a6491ce97af45835669d216c7d424cb433
yutu58 <165717@driestarleerling.nl>
2019-09-24 18:40:02 +0200

    Update timer.js

--- timer.js
+++ timer.js
@@ -43,7 +43,9 @@ var status = 0;
             if (event.keyCode == 32) {
                 if(status == 1) {
                     stop();
-		generateScramble()
+		$.getscript("main.js",function(){
+		generateScramble();
+		});
 				}
  				if (timertrue == 0) {
                 timertrue = 1;
