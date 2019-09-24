ad75803c03b3854f6ab5c1090daa2fe2cae753d4
yutu58 <165717@driestarleerling.nl>
2019-09-24 17:31:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,47 @@
 (function() {
-  'use strict';
+    var start, progressHandle;
 
-  let progressHandle;
+    var progress = function() {
+        // Add a dot each second
+        $('#status').text(function(index, text) { return text + '.'; });
+    };
 
-  function progress() {
-    // Add a dot each second
-    $('#status').text(function(index, text) {
-      return `${text} .`;
+    var initialized = function() {
+        // Precomputing finished, stop adding dots
+        clearInterval(progressHandle);
+
+        // Show the duration of initialization
+        var end = new Date,
+            duration = (end - start) / 1000;
+        $('#status').text('Initialization done in ' + duration + ' seconds.');
+
+        // Show the scrambler
+        $('#randomstate').css('visibility', 'visible');
+        $('#randomstate button').on('click', generateScramble);
+    };
+
+    var generateScramble = function() {
+        // Hide the initialization status on first scramble
+        $('#status').hide();
+
+        // Generate a scramble
+        Cube.asyncScramble(function(alg) {
+            var s = alg.replace(/\s+/g, ''),  // remove spaces
+                url = "http://cube.crider.co.uk/visualcube.png?size=150&alg=" + s;
+            $('#randomstate .result').html(alg + "<br><img src=\"" + url + "\">");
+        });
+    };
+
+    $(function() {
+        $('#status').text('Initializing');
+
+        // Start measuring time
+        start = new Date;
+
+        // Start adding dots
+        progressHandle = setInterval(progress, 1000);
+
+        // Start precomputing
+        Cube.asyncInit('/cubejs/lib/worker.js', initialized);
     });
-  }
-
-  function initialized() {
-    // Precomputing finished, stop adding dots
-    clearInterval(progressHandle);
-
-    // Show the duration of initialization
-    let end = new Date;
-    let duration = (end - start) / 1000;
-    $('#status').text(`Initialization done in  ${duration} seconds.`);
-
-    // Show the scrambler
-    $('#randomstate').css('visibility', 'visible');
-    $('#randomstate button').on('click', generateScramble);
-  }
-
-  function generateScramble() {
-    // Hide the initialization status on first scramble
-    $('#status').hide();
-
-    // Generate a scramble
-    Cube.asyncScramble(function(alg) {
-      let safeAlgo = alg.replace(/\s+/g, ''); // remove spaces
-      let url = `http://cube.crider.co.uk/visualcube.php?fmt=svg&size=150&pzl=3&alg=${safeAlgo}`;
-      $('#randomstate .result').html(`${alg}<br><img src=\"${url}\">`);
-    });
-  }
-
-  let start;
-
-  $(function() {
-    $('#status').text('Initializing');
-
-    // Start measuring time
-    start = new Date;
-
-    // Start adding dots
-    progressHandle = setInterval(progress, 1000);
-
-    // Start precomputing
-    Cube.asyncInit('./lib/worker.js', initialized);
-  });
-})();
+})()
