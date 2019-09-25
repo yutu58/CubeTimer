880dd87617de3f6279a32e22bd48f1b272eb40f3
yutu58 <165717@driestarleerling.nl>
2019-09-25 18:30:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -162,7 +162,7 @@ for (var i=0; i<times.length; i++){
     cell1 = row.insertCell()
     cell1.innerHTML = times[i]
     cell2 = row.insertCell() 
-    cell2.innerHTML = "scramble"
+    cell2.innerHTML = scrambles[i]
 }
 }
 
